becc2d5009e0a3c411f6369e47472a00ce713ae7
LAZ <sdf10528236@gmail.com>
2021-08-03 11:03:29 +0800

    bebe

--- movie_web/deploy.sh
+++ movie_web/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sdf10528236/web_movie.git main:gh-pages
+git push -f https://github.com/sdf10528236/web_movie.git master:gh-pages
 
 cd -
